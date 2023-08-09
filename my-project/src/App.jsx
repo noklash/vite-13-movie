eeac516f58f2f1aa8087a5fb9cc6835a435b06b9
noklash <123834419+noklash@users.noreply.github.com>
2023-08-09 12:42:34 +0300

    testing something

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -8,6 +8,7 @@ import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
 import AddToFavourites from './components/AddToFavourites';
 import RemoveFavourites from './components/RemoveFavourites';
+import Hamburger from '/icon-hamburger.svg'
 
 
 
@@ -15,6 +16,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState("")
   const [favourites, setFavourites] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
 
   const getMovieRequest = async () => {
@@ -28,7 +30,10 @@ const App = () => {
 		}
 	};
 
- 
+ useEffect(()=>{
+  setTimeout(()=>
+  setIsLoading(false), 6000)
+ })
 
   useEffect(() => {
 		getMovieRequest();
@@ -66,25 +71,41 @@ const App = () => {
   };
 
   return (
-    <div className='m-6 movie_app'>
-      <div>
+    <div className=' movie_app'>
+      {isLoading && <div className='loading '>
+                <h1 className='flicker'>watcham <span className='tm'>&#8482;</span></h1>
+        </div>}
+        {/* so that the loading page dispalys only loading */}
+      {!isLoading && 
+        <div className='nav'>
+      <div className='flex '>
+        <h1 className='brand mr-auto pb-6 px-4'>watcham</h1>
+        <div className='pt-4 px-4'>
+          {/* <MovieListHeading heading="Movies"/> */}
+          {/* <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/> */}
+          {window.innerWidth < 675 && <img src={Hamburger}/>}
+        </div>
+        {/* {window.innerWidth < 675 && <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>} */}
+      </div>
+      
+      {/* new stop */}
+      <div className='my-16 px-6'>
         <MovieListHeading heading="Movies"/>
-        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+        {/* testing up */}
+        <div className='flex '>
+          <MovieList 
+              movies={movies} 
+              favouriteComponent={AddToFavourites}
+              handleFavouritesClick={addFavouriteMovie}
+          />
+        </div>
       </div>
 
-      <div className='flex'>
-        <MovieList 
-            movies={movies} 
-            favouriteComponent={AddToFavourites}
-            handleFavouritesClick={addFavouriteMovie}
-        />
-      </div>
-
-      <div className='flex align-middle mt-4 mb-4'>
+      <div className='flex align-middle mt-4 mb-4 px-6'>
 				<MovieListHeading heading='Favourites' />
 			</div>
 
-      <div className=''>
+      <div className='px-6'>
 				<MovieList 
             movies={favourites} 
             favouriteComponent={RemoveFavourites}
@@ -92,7 +113,8 @@ const App = () => {
         />
 
 			</div>
-
+      </div>}
+{/* end of the is loading logic */}
     </div>
   )
 }
--- my-project/src/components/MovieListHeading.jsx
+++ my-project/src/components/MovieListHeading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MovieListHeading = ({heading}) => {
   return (
-    <div>
+    <div className='py-6 heading font-bold'>
         <h1>{heading}</h1>
     </div>
   )
